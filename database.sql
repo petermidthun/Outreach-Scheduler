081fd89806832c9b98111fc8128800181af86dd0
Peter Midthun <petermidthun@gmail.com>
2018-11-21 15:38:51 -0600

    database sql file updated

--- database.sql
+++ database.sql
@@ -1,16 +1,20 @@
 CREATE DATABASE "outreach_database";
 
-CREATE TABLE "users" (
+
+
+CREATE TABLE "users" (         --  holds passwords for all users
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL,
     "type" VARCHAR (80) DEFAULT 'instructor'
 );
 
- --  INSTRUCTOR TABLE CANNOT BE ENTERED UNTIL USERS ARE REGISTERED AND HAS MANY DEPENDENCIES
- --  REGISTER THREE USERS BEFORE CREATING ADDITIONAL TABLES
+ --  INSTRUCTOR TABLE CANNOT BE CREATED UNTIL USERS ARE REGISTERED AND HAS MANY DEPENDENCIES
+ --  REGISTER USERS BEFORE CREATING ADDITIONAL TABLES
 
-CREATE TABLE "clients" (
+
+
+CREATE TABLE "clients" (    --  Holds information on clients that might purchase programming
     "client_id" SERIAL PRIMARY KEY,
     "name" VARCHAR (255),
     "address" VARCHAR (255),
@@ -22,8 +26,11 @@ CREATE TABLE "clients" (
 INSERT INTO "clients" ("name", "address", "site_contact", "billing_contact", "call_out_information")
 VALUES 
 ('Happy Apple Charter', '10800 Brookings ave, mpls, 55108', 'Angela - 432 555 4334', 'Jaquan - 643 5544 3546', 'call out info'),
-('Saint Anthony Middle', '1080 Lincoln st, mpls, 55108', 'Marquis - 432 555 4334', 'Lakeesha - 643 5544 3546', 'call out info'),
-('Kitson County', '1555 spruce st, st paul, 55348', 'Alondra - 432 555 4334', 'tank - 643 5544 3546', 'call out info');
+('Kitson County', '1555 spruce st, st paul, 55348', 'Alondra - 432 555 4334', 'tank - 643 5544 3546', 'call out info'),
+('German Immerson', '2222 pine st, st paul, 55444', 'Vasquez - 432 555 4003', 'myrtle - 643 5544 3006', 'call out info'),
+('Saint Anthony Middle', '100 Lincoln st, mpls, 55108', 'Marquis - 432 555 4334', 'Lakeesha - 643 5544 3546', 'call out info');
+
+--  Holds list of instructors and tracks how many hours they have spent presenting programs
 
 CREATE TABLE "instructors" (
     "instructor_id" SERIAL PRIMARY KEY,
@@ -38,6 +45,8 @@ VALUES
 ('Sue', 115, 2),
 ('keesha', 55, 3);
 
+--  Holds a list of programs that might be purchased/presented
+
 CREATE TABLE "programs" (
     "program_id" SERIAL PRIMARY KEY,
     "name" VARCHAR (255)
@@ -46,43 +55,35 @@ CREATE TABLE "programs" (
 INSERT INTO "programs" ("name")
 VALUES ('Electricity'),('Water'), ('Engineering');
 
+--  Holds a list of vans available to deliver programming 
 
 CREATE TABLE "vans" (
-    "program_id" SERIAL PRIMARY KEY,
+    "van_id" SERIAL PRIMARY KEY,
     "color" VARCHAR (255)
 );
 
 INSERT INTO "vans" ("color")
 VALUES ('Red'),('Green'), ('Purple');
 
+--  Holds a list of bookings for each client
+--  One booking may consist of multiple dates and program instances 
+--  Resources (vans, intructors, etc) are assigned to dates and program instances.
+--  See ERD.
+
 CREATE TABLE "bookings" (
     "booking_id" SERIAL PRIMARY KEY,
     "client_id" INT REFERENCES "clients",
     "booking_note" TEXT,
-    "callout" boolean DEFAULT FALSE,
-    "thankyou" boolean DEFAULT FALSE
+    "callout" boolean DEFAULT FALSE,    --  has client been called for this booking
+    "thankyou" boolean DEFAULT FALSE,   --  has client been thanked after this booking
+    "tourorovernight" boolean DEFAULT FALSE   --  is this booking an overnight or regional tour
 );
 
-INSERT INTO "bookings" ("client_id", "booking_note", "callout", "thankyou") 
-VALUES (1, 'Big paragraphs here', FALSE, FALSE), (1, 'big paragraphs', FALSE, FALSE), (2, 'big paragraphs', FALSE, FALSE), (2, 'booking notes', FALSE, FALSE), (3, 'more notes', FALSE, FALSE), (3, 'booking notes', FALSE, FALSE), (2, 'more booking notes', FALSE, FALSE);
+INSERT INTO "bookings" ("client_id", "booking_note") 
+VALUES (1, 'elec, eng 7, 9, 3/23, keesha, Happy Apple'), (2, 'lots o waters, 4/21+4/22+4/23, Peter and Sue, kittson'), (3, 'Eng, 3/11/17 german'), (3, 'Elec, 3/20/18 german'), (4, 'water, anthony, 2016'), (4, 'water, anthony, 2017'), (4, 'water, anthony, 2018');
 
 
-
-CREATE TABLE "previous_visits" (
-    "visit_id" SERIAL PRIMARY KEY,
-    "client_id" INT REFERENCES "clients",
-    "instructor_id" INT REFERENCES "instructors",
-    "date" DATE
-);
-
-INSERT INTO "previous_visits" ("client_id", "instructor_id", "date") 
-VALUES (1, 1, '2017-03-22'), (1, 3, '2018-04-22' ), 
-        (2, 2, '2017-03-02' ), (2, 1, '2018-03-12' ),
-        (3, 3, '2017-03-25' ), (3, 2, '2018-04-22' ),
-        (1, 2, '2016-03-22' ), (2, 3, '2016-03-22' );
-
-
-CREATE TABLE "program_feedback" (
+CREATE TABLE "program_feedback" ( --  holds feedback submitted by instructors
     "feedback_id" SERIAL PRIMARY KEY,
     "program_id" INT REFERENCES "programs",
     "instructor_id" INT REFERENCES "instructors",
@@ -95,7 +96,7 @@ VALUES (1, 1, 'Giant test tube broke'), (1, 3, 'Out of flame paper' ),
         (3, 3, 'bird train is awesome!' ), (3, 2, 'dont overheat eggs'),
         (1, 2, 'conclusion is too long'), (2, 3, 'needs shortening' );
 
-CREATE TABLE "van_issues" (
+CREATE TABLE "van_issues" (     --  holds van issues submitted by instrutors
     "issue_id" SERIAL PRIMARY KEY,
     "van_id" INT REFERENCES "vans",
     "instructor_id" INT REFERENCES "instructors",
@@ -108,16 +109,22 @@ INSERT INTO "van_issues" ("van_id", "instructor_id", "date_submitted", "issue")
 VALUES (1, 1, '2018-3-22','check engine light'), (1, 3, '2018-3-23', 'brakes squeeking' ), 
         (2, 2, '2018-3-23', 'front left tire low' ), (3, 1, '2018-3-24', 'big scratch, left side' );
 
+--  Holds dates associated with particular bookings
+
 CREATE TABLE "booking_dates" (
     "booking_date_id" SERIAL PRIMARY KEY,
     "booking_id" INT REFERENCES "bookings",
-    "date" DATE
+    "date" DATE,
+    "van_id" INT REFERENCES "vans"
 );
 
-INSERT INTO "booking_dates" ("booking_id", "date") 
-VALUES (1, '2017-03-23' ), (1, '2018-04-21'), (1, '2018-04-22' ), (1, '2018-04-23' ),
-        (2, '2017-03-11' ), (2, '2018-03-20' ),
-        (3, '2016-03-25' ), (3, '2017-04-22' ), (3, '2018-03-22' );
+INSERT INTO "booking_dates" ("booking_id", "date", "van_id") 
+VALUES (1, '2017-03-23', 1), 
+        (2, '2018-04-21', 2), (2, '2018-04-22', 2), (2, '2018-04-23', 2),
+        (3, '2017-03-11', 3), (4, '2018-03-20', 3),
+        (5, '2016-03-25', 2 ), (6, '2017-04-22', 1 ), (7, '2018-03-22', 1 );
+
+--  Holds program instances associated with particular dates
 
 CREATE TABLE "program_instances" (
     "instance_id" SERIAL PRIMARY KEY,
@@ -138,16 +145,32 @@ VALUES
 (6, 1, 2, '011:00:00', FALSE ),  
 (7, 2, 1, '010:00:00', FALSE ), 
 (8, 2, 3, '010:00:00', FALSE ),
-(8, 2, 2, '010:00:00', FALSE );
+(9, 2, 2, '010:00:00', FALSE );
 
 
-CREATE TABLE "van_bookings" (
-    "van_booking_id" SERIAL PRIMARY KEY,
-    "booking_date_id" INT REFERENCES "booking_dates",
-    "van_id" INT REFERENCES "vans"
-);
 
-INSERT INTO "van_bookings" ("booking_date_id", "van_id") 
-VALUES (1, 1 ), (2, 2), (3, 2 ), (4, 2 ),
-        (5, 3 ), (6, 3 ),
-        (7, 2 ), (8, 1 ), (9, 1 );
+-- CREATE TABLE "previous_visits" (
+--     "visit_id" SERIAL PRIMARY KEY,
+--     "client_id" INT REFERENCES "clients",
+--     "instructor_id" INT REFERENCES "instructors",
+--     "date" DATE
+-- );
+
+-- INSERT INTO "previous_visits" ("client_id", "instructor_id", "date") 
+-- VALUES (1, 1, '2017-03-22'), (1, 3, '2018-04-22' ), 
+--         (2, 2, '2017-03-02' ), (2, 1, '2018-03-12' ),
+--         (3, 3, '2017-03-25' ), (3, 2, '2018-04-22' ),
+--         (1, 2, '2016-03-22' ), (2, 3, '2016-03-22' );
+
+
+--  Instructor Calendar View
+SELECT programs.name, booking_dates.date, program_instances.time, clients.name, vans.color as van, bookings.callout, bookings.thankyou, bookings.booking_note, bookings.touron
+FROM program_instances 
+JOIN booking_dates ON program_instances.booking_date_id=booking_dates.booking_date_id
+JOIN bookings ON booking_dates.booking_id=bookings.booking_id
+JOIN clients ON bookings.client_id=clients.client_id
+JOIN programs ON program_instances.program_id=programs.program_id
+JOIN vans ON booking_dates.van_id=vans.van_id
+JOIN instructors ON program_instances.instructor_id=instructors.instructor_id
+WHERE instructors.instructor_id = 1;
+
